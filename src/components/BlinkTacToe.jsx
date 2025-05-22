427133a6ecd75f04f8ce6fcb0ef985f9f9c2d77b
Aditya Mathur <aditya.2024@Adityas-MacBook-Pro-7.local>
2025-05-22 13:24:39 +0530

    Header and some files created

---
+++ src/components/BlinkTacToe.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Header from "./Header";
+
+export default function BlinkTacToe(){
+    return (
+        <div className="game-container">
+            <Header />
+        </div>
+    )
+}
---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import '../styles/globals.css'
+
+export default function Header(){
+    return (
+        <div className="header">
+            <h1 className="title">Blink Tac Toe</h1>
+            <p className="subtitle">Where emojis vanish and strategy matters !!</p>
+            <p className="subtitle">By: Aditya Mathur</p>
+        </div>
+    )
+}
